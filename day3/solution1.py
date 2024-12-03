b3d6a46e1255881ad4843d478f8e97f9b43cb83e
aledstephens <aledstephens@gmail.com>
2024-12-03 15:09:43 +0000

    day 3, part 2 attempt. not correct

--- day3/solution1.py
+++ day3/solution1.py
@@ -1,5 +1,6 @@
 import re
 
+# PART 1 Functions
 def import_data(file_path: str) -> list[str]:
     with open(file_path, 'r') as file:
         data = [line for line in file]
@@ -10,8 +11,11 @@ def extract_mul(input: str) -> list[tuple[str,str]]:
     '''
     Match this pattern: mul(X,Y) where X and Y are numbers
     '''
+
     pattern = r'mul\((\d+),(\d+)\)' # r is for raw string, \d is for digits, + is for 1 or more
+
     matches = re.findall(pattern, input)
+
     return matches
 
 
@@ -25,6 +29,76 @@ def mul_tuples_and_sum(input: list[tuple[str,str]]) -> list[int]:
     result = sum(result)
     return result
 
+### Solution to Part 1
+# if __name__ == "__main__":
+#     file_path = 'day3/input_data/input_data.txt'
+#     input = import_data(file_path)
+#
+#     total = 0
+#
+#     for line in input:
+#         matches = extract_mul(line)
+#         result = mul_tuples_and_sum(matches)
+#         total += result
+#
+#     print(total)
+
+
+# PART 2 Functions:
+# idea is to split on don't(), insert a marker, split on do(), remove item directly after marker, and return string
+def split_str_on_dont(input: str) -> list[str]:
+    split_string = input.split("don't()")
+    return split_string
+
+
+def insert_between(lst: list[str]) -> list[str]:
+    insert_str = 'ignore_next'
+    result = []
+    for item in lst:
+        result.append(item)
+        result.append(insert_str)
+    return result
+
+
+def split_str_on_do(input: list[str]) -> list[str]:
+    result = []
+    for item in input:
+        split_string = item.split("do()")
+        result.append(split_string)
+    return result
+
+
+
+def flatten_list_to_list(input: list[list[str]]) -> list[str]:
+    result = []
+    for item in input:
+        for sub_item in item:
+            result.append(sub_item)
+    return result
+
+
+def remove_incompatible_items(split_string: list[str]) -> list[str]:
+    result = []
+    for index, item in enumerate(split_string):
+        if item == 'ignore_next':
+            if index + 1 == len(split_string):
+                continue
+            else:
+                split_string.pop(index + 1) # skip the next item
+        else:
+            result.append(item)
+    return result
+
+
+def flatten_list_to_string(input: list[str]) -> str:
+    result = ''
+    for item in input:
+        result += item
+    return result
+
+
+
+### Solution to Part 2
 if __name__ == "__main__":
     file_path = 'day3/input_data/input_data.txt'
     input = import_data(file_path)
@@ -32,9 +106,14 @@ if __name__ == "__main__":
     total = 0
 
     for line in input:
-        matches = extract_mul(line)
+        split_string = split_str_on_dont(line)
+        split_string = insert_between(split_string)
+        split_string = split_str_on_do(split_string)
+        split_string = flatten_list_to_list(split_string)
+        split_string = remove_incompatible_items(split_string)
+        split_string = flatten_list_to_string(split_string)
+        matches = extract_mul(split_string)
         result = mul_tuples_and_sum(matches)
         total += result
 
     print(total)
-
--- tests/test_day3.py
+++ tests/test_day3.py
@@ -1,4 +1,4 @@
-from day3.solution1 import extract_mul, mul_tuples_and_sum
+from day3.solution1 import *
 
 def test_extract_mul():
     input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
@@ -8,6 +8,7 @@ def test_extract_mul():
 
     assert result == desired
 
+
 def test_mul_tuples_and_sum():
     input = [('2', '4'), ('5', '5'), ('11', '8'), ('8', '5')]
     desired = 161
@@ -16,3 +17,72 @@ def test_mul_tuples_and_sum():
 
     assert result == desired
 
+def test_split_string_on_dont():
+    input = "a string with 2 don't()s in it, also don't() here"
+    desired = ['a string with 2 ',  's in it, also ', ' here']
+
+    result = split_str_on_dont(input)
+
+    assert result == desired
+
+def test_insert_between():
+    input = ['a string with 2 ', 's in it, also ', ' here']
+    desired = ['a string with 2 ', 'ignore_next', 's in it, also ', 'ignore_next', ' here', 'ignore_next']
+
+    result = insert_between(input)
+
+    assert result == desired
+
+def test_part_2():
+    input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+    split_string = split_str_on_dont(input)
+    split_string = insert_between(split_string)
+    split_string = split_str_on_do(split_string)
+    split_string = flatten_list_to_list(split_string)
+    split_string = remove_incompatible_items(split_string)
+    split_string = flatten_list_to_string(split_string)
+    matches = extract_mul(split_string)
+    result = mul_tuples_and_sum(matches)
+
+    assert result == 48
+
+def test_part_2_mod():
+    input = "do()xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+do()..do()mul(32,64)dont()..don't()(mul(11,8)undo()?mul(8,5))"
+    split_string = split_str_on_dont(input)
+    split_string = insert_between(split_string)
+    split_string = split_str_on_do(split_string)
+    split_string = flatten_list_to_list(split_string)
+    split_string = remove_incompatible_items(split_string)
+    split_string = flatten_list_to_string(split_string)
+    matches = extract_mul(split_string)
+    result = mul_tuples_and_sum(matches)
+
+    desired = (2*4)+(32*64)+(8*5)
+    assert result == desired
+
+def test_part_2_dont_at_start():
+    input = "don't()xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+    split_string = split_str_on_dont(input)
+    split_string = insert_between(split_string)
+    split_string = split_str_on_do(split_string)
+    split_string = flatten_list_to_list(split_string)
+    split_string = remove_incompatible_items(split_string)
+    split_string = flatten_list_to_string(split_string)
+    matches = extract_mul(split_string)
+    result = mul_tuples_and_sum(matches)
+
+    assert result == 40
+
+def test_part_2_consecutive_donts():
+    input = "mul(1,2)don't()do()mul(3,4)don't()don't()mul(5,6)don't()mul(7,8)do()mul(9,10)don't()do()don't()"
+    split_string = split_str_on_dont(input)
+    split_string = insert_between(split_string)
+    split_string = split_str_on_do(split_string)
+    split_string = flatten_list_to_list(split_string)
+    split_string = remove_incompatible_items(split_string)
+    split_string = flatten_list_to_string(split_string)
+    matches = extract_mul(split_string)
+    result = mul_tuples_and_sum(matches)
+
+    desired = (1*2)+(3*4)+(9*10)
+    assert result == desired
