ebd324aeda4155b858e929e8deb32b3af8147dcc
aledstephens <aledstephens@gmail.com>
2024-12-04 21:35:16 +0000

    day1: tidied data parsing to avoid using csv / pandas

--- day1/solution.py
+++ day1/solution.py
@@ -1,47 +1,60 @@
 import numpy as np
-import pandas as pd
 from collections import Counter
 
-# load input data
-def load_data():
-    '''
-    Required manually saving 2 csv files which doesn't seem ideal.
-    '''
-    df1 = pd.read_csv('day1/input_data/list1.csv', header=None)
-    df2 = pd.read_csv('day1/input_data/list2.csv', header=None)
-    list1 = df1[0].tolist()
-    list2 = df2[0].tolist()
-    return list1, list2
 
-def compute_distance(list1, list2):
-    list1.sort()
-    list2.sort()
-    distance = abs(np.subtract(list1, list2))
+def load_input(file_path: str) -> list:
+
+    with open(file_path) as f:
+        data = f.read().splitlines()
+
+    return data
+
+
+def parse_input(data: list) -> tuple:
+    '''
+    Converts input to two lists of alternating integers
+    '''
+    lst1 = []
+    lst2 = []
+
+    for line in data:
+        values = line.split('   ') # why is input seperator 3 spaces?
+        lst1.append(int(values[0]))
+        lst2.append(int(values[1]))
+
+    return lst1, lst2
+
+
+def compute_distance(lst1, lst2):
+    lst1.sort()
+    lst2.sort()
+    distance = abs(np.subtract(lst1, lst2))
     total = np.sum(distance)
     return total
 
-def similarity_score(list1, list2):
+
+def similarity_score(lst1, lst2):
     '''
     After much faffing with numpy and pandas,
     I discovered the Counter class in the collections module,
     it made the solution much tidier.
     '''
-    counts = Counter(list2)
+    counts = Counter(lst2) # count the occurrences of each element in list2
 
     total = 0
 
-    for element in list1:
+    for element in lst1:
         if element in counts:
-            total += element * counts[element]
+            total += element * counts[element] # multiply element by its count in list2
     return total
 
 
 if __name__ == "__main__":
-    list1, list2 = load_data()
+    input_data = load_input(file_path= 'day1/input_data/input.txt')
+    list1, list2 = parse_input(input_data)
+
     result1 = compute_distance(list1, list2)
     print(f'distance total: {result1}')
+
     result2 = similarity_score(list1, list2)
     print(f'similarity score: {result2}')
-
-
-
