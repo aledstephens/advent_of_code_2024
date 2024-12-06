e76a4553db98c9c7a631f643c4b5a45b74407250
aledstephens <aledstephens@gmail.com>
2024-12-06 11:09:25 +0000

    day5: part 2 complete

--- day5/solution.py
+++ day5/solution.py
@@ -1,7 +1,9 @@
-# import rules
+'''
+Part 1 is a validity check
+Part 2 is a fixes the updates by running each one through the rules backwards and forwards until all rules pass.
+'''
 
 def load_input(file_path: str) -> list:
-
     with open(file_path) as f:
         data = f.read().splitlines()
 
@@ -14,16 +16,17 @@ def get_fore_aft(rule: str) -> tuple:
 
 
 def move_aft(update: list, fore_index: int, aft: str) -> list:
-    # print(f'Reordering update: {update} by moving {aft} before {fore}')
     update.remove(aft)
     update.insert(fore_index, aft)
+
     return update
+
 
 def check_validity(rules: list, updates: list) -> dict:
     validity_map = {}
 
     for update in updates:
-        update = update.split(",")
+        update = update.split(",") # convert to list
         valid_update = True
 
         for rule in rules:
@@ -33,18 +36,15 @@ def check_validity(rules: list, updates: list) -> dict:
             if fore not in update or aft not in update:
                 continue
 
-            # get index of first occurrence of fore and aft in update
+            # get index of first occurrence of fore and aft in update and check order
             fore_index = update.index(fore)
             aft_index = update.index(aft)
 
-            # check if fore comes before aft
             if fore_index > aft_index:
                 valid_update = False
 
-        # convert update back to string
-        update = ",".join(update)
+        update = ",".join(update) # convert back to string
 
-        # update validity map
         if valid_update:
             validity_map[update] = True
         else:
@@ -52,49 +52,55 @@ def check_validity(rules: list, updates: list) -> dict:
 
     return validity_map
 
-def fix_update(rules: list, update: list) -> dict:
+
+def get_invalid_updates(validity_map: dict) -> list:
+    return [key for key in validity_map if validity_map[key] == False]
+
+
+def fix_updates(rules: list, updates: list) -> dict:
     """
     To fix, keep running the update through the rules backwards and forwards until it's valid.
     """
+    fixed_updates_map = {}
 
-    # print(f'Fixing update: {update}')
-    update = update.split(",")
+    for update in updates:
 
-    invalid = True
+        update = update.split(",")
 
-    while invalid:
+        invalid_update = True
 
-        invalid_count = 0
-        rules = rules[::-1] # reverse rules
+        while invalid_update:
 
-        for rule in rules:
+            invalid_rules_count = 0
+            rules = rules[::-1] # reverse rules
 
-            fore, aft = get_fore_aft(rule)
+            for rule in rules:
 
-            if fore not in update or aft not in update:
-                continue
+                fore, aft = get_fore_aft(rule)
 
-            fore_index = update.index(fore)
-            aft_index = update.index(aft)
-            # print(f'found fore at index: {fore_index}. found aft at index: {aft_index}')
+                if fore not in update or aft not in update:
+                    continue
 
-            if fore_index > aft_index:
-                invalid_count += 1
-                update = move_aft(update, fore_index, aft) # try fix
-                continue
+                fore_index = update.index(fore)
+                aft_index = update.index(aft)
 
-        if invalid_count == 0:
-            invalid = False
+                if fore_index > aft_index:
+                    invalid_rules_count += 1
+                    update = move_aft(update, fore_index, aft) # try fix
+                    continue
+
+            if invalid_rules_count == 0:
+                invalid_update = False
+
+        # convert update back to string
+        update = ",".join(update)
+
+        fixed_updates_map[update] = True
+
+    return fixed_updates_map
 
 
-    # convert update back to string
-    update = ",".join(update)
-
-    return update
-
-
-def sum_mid_pages(validity_map: dict) -> int:
-    # get true keys from validity map
+def sum_mid_pages(validity_map: dict) -> tuple:
     valid_updates = [key for key in validity_map if validity_map[key] == True]
     valid_updates_count = len(valid_updates)
 
@@ -109,29 +115,20 @@ def sum_mid_pages(validity_map: dict) -> int:
 
 
 if __name__ == "__main__":
-
+    #Load data
     rules = load_input("day5/input_data/rules-full.txt")
     updates = load_input("day5/input_data/updates-full.txt")
 
     #Part 1
-    print('PART 1')
-    print(f'No of updates: {len(updates)}')
+    print(f'PART 1: No of updates: {len(updates)}')
     validity_map = check_validity(rules, updates)
     result, valid_count = sum_mid_pages(validity_map)
-    print(f'No of valid updates: {valid_count}')
-    print(f'Sum of mid pages: {result}') # 143 / 5762
+    print(f'No of valid updates: {valid_count}. Sum of mid pages: {result}') # 143 / 5762
 
     #Part 2
-    print('PART 2')
-    invalid_updates = [key for key in validity_map if validity_map[key] == False]
-    print(f'No of invalid updates: {len(invalid_updates)}')
-
-    fixed_updates_map = {}
-    for index, update in enumerate(invalid_updates):
-        print(f'Fixing update {index+1} of {len(invalid_updates)}: {update}')
-        fixed_updates_map[fix_update(rules, update)] = True
-
+    invalid_updates = get_invalid_updates(validity_map)
+    print(f'PART 2: No of invalid updates: {len(invalid_updates)}')
+    fixed_updates_map = fix_updates(rules, invalid_updates)
     result, valid_count = sum_mid_pages(check_validity(rules, fixed_updates_map))
-    print(f'No of valid updates: {valid_count}')
-    print(f'Sum of mid pages: {result}') # 123 / 4130
+    print(f'After fixing, sum of mid pages: {result}') # 123 / 4130
 
