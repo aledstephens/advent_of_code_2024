0c3b978cbbb0bb4c9f07fec94ab91c23b096c82c
aledstephens <aledstephens@gmail.com>
2024-12-13 12:55:24 +0000

    day11: part 2 complete

--- day11/solution.py
+++ day11/solution.py
@@ -1,59 +1,86 @@
 """
-Day 11 - Part 1
+Day 11 - Part 1 & Part 2
+Trick to get part 2 to run efficiently was to use a map to record count of each stone,
+rather than holding value of each stone individually.
+
+Rules:
 # rule 1 - find 0s and make 1
 # rule 2 - split even digits
 # rule 3 - multiply by 2024
 """
-
-
 
 def split_even_numbers(stone, no_digits):
     first_part = str(stone)[:(int(no_digits / 2))]
     second_part = str(stone)[int((no_digits / 2)):]
     return int(first_part), int(second_part)
 
+
+def add_to_map(stone, count, final_map):
+    if stone in final_map:
+        final_map[stone] += count
+    else:
+        final_map[stone] = count
+
+    return final_map
+
+
+def create_map_from_list(arrangement):
+    map = {}
+
+    for item in arrangement:
+        if item in map:
+            map[item] += 1
+        else:
+            map[item] = 1
+
+    return map
+
+
 def run_algorithm(initial_arrangement, total_blinks):
+
+    initial_map = create_map_from_list(initial_arrangement) # create map of stone value and count
 
     for blink in range(1, total_blinks + 1):
         print(f"blink no. {blink} of {total_blinks}")
 
-        final_arrangement = []
+        final_map = {}
 
-        for stone in initial_arrangement:
+        for key, value in initial_map.items():
+            stone = key
+            count = value
+            # print(f"stone: {stone}, count: {count}")
 
             if stone == 0:
-                # print(f"stone: {stone} is 1. making it zero")
-                final_arrangement.extend([1])
+                # print(f"there are {count} stone value {stone}. making them 1s")
+                final_map = add_to_map(1, count, final_map)
                 continue
 
             no_digits = len(str(stone))
 
             if no_digits % 2 == 0:
                 first_part, second_part = split_even_numbers(stone, no_digits)
-                # print(f"stone: {stone} has even numbers. first part: {first_part}, second part: {second_part}")
-                final_arrangement.extend([first_part, second_part])
+                # print(f"there are {count} stone value {stone}. They have even numbers. Making first part: {first_part} & second part: {second_part}")
+                final_map = add_to_map(first_part, count, final_map)
+                final_map = add_to_map(second_part, count, final_map)
 
             else:
-                # print(f"stone: {stone} is not one and has odd numbers. multiplying by 2024")
-                final_arrangement.extend([stone * 2024])
+                # print(f"There are {count} stone value {stone}. Is 'other' value so multiplying by 2024")
+                stone *= 2024
+                final_map = add_to_map(stone, count, final_map)
 
-        print(f"Blink {blink}, final stones: {len(final_arrangement)}")
+        print(f"Blink {blink}, final stone values: {len(final_map)}, total stones: {sum(final_map.values())}")
         # replace arrangement
-        initial_arrangement = final_arrangement
+        initial_map = final_map
 
-    return final_arrangement
+    return final_map
+
 
 if __name__ == "__main__":
 
     initial_arrangement = [890, 0, 1, 935698, 68001, 3441397, 7221, 27]
-    total_blinks = 25 #simply changing to 75 for part 2 will take a looooong time.
+    total_blinks = 75
 
     result = run_algorithm(initial_arrangement, total_blinks)
-    print(len(result))
 
-
-
-
-
-
-
+    total_stones = sum(result.values())
+    print(f"Total stones: {total_stones}")
--- tests/test_day11.py
+++ tests/test_day11.py
@@ -9,8 +9,47 @@ def test_split_evens():
     number = 1000
     assert split_even_numbers(number, len(str(number))) == (10, 0)
 
+
 def test_run_algorithm():
-    initial_arrangement = [125, 17]
+    initial_arrangement = {125:1, 17:1}
     total_blinks = 6
-    result = [2097446912, 14168, 4048, 2, 0, 2, 4, 40, 48, 2024, 40, 48, 80, 96, 2, 8, 6, 7, 6, 0, 3, 2]
+    result = {
+        2097446912: 1,
+        14168: 1,
+        4048: 1,
+        2: 4,
+        0: 2,
+        4: 1,
+        40: 2,
+        48: 2,
+        2024: 1,
+        80: 1,
+        96: 1,
+        8: 1,
+        6: 2,
+        7: 1,
+        3: 1
+    }
+
     assert run_algorithm(initial_arrangement, total_blinks) == result
+
+def test_run_algorithm_2():
+    initial_arrangement = [0, 0, 1, 1, 1000, 1000]
+    total_blinks = 2
+
+    # result_1_blink = {
+    #     1: 2,
+    #     2024: 2,
+    #     10: 2,
+    #     0: 2
+    # }
+
+    result_2_blink = {
+        2024: 2,
+        20: 2,
+        24: 2,
+        1: 4,
+        0: 2,
+    }
+
+    assert run_algorithm(initial_arrangement, total_blinks) == result_2_blink
